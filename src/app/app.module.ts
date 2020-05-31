db81397add7de399838d8c619eb1a9dc1816597e
maxdyachuk <dyachukmv1@gmail.com>
2020-05-31 17:21:23 +0300

    add components and fill with dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeDetailsComponent} from './recipes/recipe-details/recipe-details.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipesComponent,
+    RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('A', 5),
+    new Ingredient('B', 3)
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
